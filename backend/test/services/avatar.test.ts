5096145fa510d518c41d7fd6ff7c337b3bc34a0f
Muri <76880145+muridot0@users.noreply.github.com>
2022-11-30 21:04:38 +0000

    Delete backend directory

--- backend/test/services/avatar.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe('\'avatar\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('avatar');
-    expect(service).toBeTruthy();
-  });
-});
--- backend/test/services/space.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe('\'space\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('space');
-    expect(service).toBeTruthy();
-  });
-});
