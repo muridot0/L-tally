2afad1b57a7befce9f14d24b0f4cf396be9c578e
muridot0 <muri0@outlook.com>
2022-11-30 20:49:03 +0000

    initial commit

---
+++ backend/src/models/users.model.ts
@@ -0,0 +1,32 @@
+// users-model.ts - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+import { Application } from '../declarations';
+import { Model, Mongoose } from 'mongoose';
+import  avatar  from './avatar.model';
+
+export default function (app: Application): Model<any> {
+  const avatarSchema = avatar(app);
+  const modelName = 'users';
+  const mongooseClient: Mongoose = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const schema = new mongooseClient.Schema({
+  
+    email: { type: String, unique: true, lowercase: true },
+    password: { type: String },
+    avatar: avatarSchema.schema,
+    lTally: Number,
+    Space: { type: Schema.Types.ObjectId }
+  
+  }, {
+    timestamps: true
+  });
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    (mongooseClient as any).deleteModel(modelName);
+  }
+  return mongooseClient.model<any>(modelName, schema);
+}
