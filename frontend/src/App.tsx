42cca18078bcef173443ce8ad65f8e731fbcf9b0
muridot0 <muri0@outlook.com>
2023-01-22 18:34:06 +0000

    adding login page and some touch-ups

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { SpaceContext } from './contexts/space';
 import DefaultPage from './Pages/DefaultPage/DefaultPage';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
   const [activeMenuItem, setActiveMenuItem] = useState('');
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />}>
           <Route path=':space' element={<DefaultPage />} />
         </Route>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </SpaceContext.Provider>
   );
---
+++ frontend/src/Pages/Login/Login.tsx
@@ -0,0 +1,47 @@
+import styles from './Login.module.css';
+import clsx from 'clsx';
+
+export default function Login() {
+  return (
+    <>
+      <div className={styles.logo}>
+        <span className={clsx('material-symbols-rounded', styles.icon)}>
+          scoreboard
+        </span>
+        <div>
+          <p className={styles.headerTop}>L-Tally</p>
+          <p className={styles.headerBottom}>Keeping score ...</p>
+        </div>
+      </div>
+      <div className={styles.login}>
+        <div className={styles.loginCard}>
+          <div className={styles.loginLogo}>
+            <span className={clsx('material-symbols-rounded', styles.icon)}>
+              scoreboard
+            </span>
+            <p className={styles.headerTop}>L-Tally</p>
+          </div>
+          <h2 className={styles.loginHeader}>Log in</h2>
+          <form className={styles.inputs}>
+            <label htmlFor='name'>Full name</label>
+            <input type='text' id='name' />
+            <label htmlFor='email'>Email address</label>
+            <input type='text' id='email' />
+            <label htmlFor='password'>Password</label>
+            <input type='text' id='password' />
+            <div className={styles.submit}>
+              <button>Log in</button>
+            </div>
+          </form>
+          <div className={styles.toggleSignIn}>
+            <p>Not registered yet? </p>
+            <a href='/'> Create an account </a>
+            <span className={clsx('material-symbols-rounded')}>
+              arrow_forward
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
